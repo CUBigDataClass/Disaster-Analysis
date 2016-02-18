5abbcc46ad52bf89f6f858e34938fd5f98f3be06
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-02-18 11:06:36 -0700

    Added file to extract Tweet and Store in MongoDB

---
+++ extractTweets.py
@@ -0,0 +1,32 @@
+import tweepy
+
+#user application credentials
+consumer_key="PROVIDE INFO HERE"
+consumer_secret="PROVIDE INFO HERE"
+
+access_token="PROVIDE INFO HERE"
+access_token_secret="PROVIDE INFO HERE"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+class CustomStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        super(tweepy.StreamListener, self).__init__()
+
+    def on_status(self, status):
+        print status.text , "\n"
+
+    #handle errors without closing stream:
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True 
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True 
+
+sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+sapi.filter(track=['joke'])
