16620828ab6b8b560e9b36681524a2c773e77321
root <root@ip-172-31-29-136.us-west-2.compute.internal>
2016-03-29 23:36:05 +0000

    d3 folder created

---
+++ flaskapp/static/d3/graph.js
@@ -0,0 +1,11 @@
+/*d3.json("data.js", function(data) 
+	{*/
+	//	alert("Hello")
+//	});
+//
+d3.json("",function(data)
+{
+	//var content = {{content|tojson}}
+	console.log(keysOutput);
+	console.log(valuesOutput);
+})
