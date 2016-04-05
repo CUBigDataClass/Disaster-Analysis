05bb448b89127f1b8f24b6051ab2f5a890c9541c
EC2 Default User <ec2-user@ip-172-31-29-136.us-west-2.compute.internal>
2016-04-05 21:29:39 +0000

    Modified code to check previous process ID correctly

--- restartServer.sh
+++ restartServer.sh
@@ -9,7 +9,7 @@ if [[ $previousProcessId == "" ]]; then
 	python /home/ec2-user/Disaster-Analysis/storeTweetsInMongoDBUsingTweepy.py &
 	echo "$!"  > /home/ec2-user/Disaster-Analysis/.processID
 elif [[ $previousProcessId != "" ]]; then
-	checkProcessID=`echo $(ps -ef | egrep -i $previousProcessId | grep -iv "grep" | wc -l )`
+	checkProcessID=`echo $(ps -ef | egrep -iw $previousProcessId | grep -iv "grep" | wc -l )`
 	if [[ $checkProcessID == 0 ]]; then
 		source /home/ec2-user/Disaster-Analysis/venv/bin/activate
 		python /home/ec2-user/Disaster-Analysis/storeTweetsInMongoDBUsingTweepy.py &
